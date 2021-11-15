6f16c727939698af215c25f93234849347bc7cdd
Shota Mamulashvili <mamulashvili13@gmail.com>
2021-11-15 20:15:11 +0400

    Laravel admin database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Encore\Admin\Auth\Database\AdminTablesSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            AdminTablesSeeder::class,
+        ]);
     }
 }
